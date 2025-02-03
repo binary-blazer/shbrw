ab5fa3e0a2b9048368555a905572d0424115a1b4
Jonas Franke <jonasfranke@sdevs.org>
2025-02-03 21:58:41 +0100

    Add build workflow and example project

---
+++ example/index.js
@@ -0,0 +1,19 @@
+const { takeScreenshot } = require('scb');
+
+const url = 'https://example.com';
+const options = {
+    width: 1920,
+    height: 1080,
+    x: 0,
+    y: 0,
+    noJS: false
+};
+
+takeScreenshot(url, options)
+    .then(buffer => {
+        // Do something with the screenshot buffer
+        console.log('Screenshot taken successfully');
+    })
+    .catch(error => {
+        console.error('Error taking screenshot:', error);
+    });
